94073b588d1ea83cf74da206d012240a8ed64d74
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-06-17 11:29:15 +0900

    Create PlayerSpawn.cs
    プレイヤーをゲーム開始時にスポーンするスクリプトを作成した。

---
+++ Unlucky/Assets/Member/Ichihara/Scripts/PlayerSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _playerPoints = new List<GameObject>();
+
+    // Player のプレハブ
+    [SerializeField]
+    private GameObject _player1 = null;
+    [SerializeField]
+    private GameObject _player2 = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(_player1, _playerPoints[0].transform.position, Quaternion.identity);
+        Instantiate(_player2, _playerPoints[2].transform.position, Quaternion.identity);
+    }
+}
