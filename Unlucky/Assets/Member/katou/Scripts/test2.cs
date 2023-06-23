350238fc222b1a292b9ce830d23f99123d2bf5e8
katoukanato <104349883+katoukanato@users.noreply.github.com>
2023-06-23 10:13:35 +0900

    キーを同時に押したらと時間経過でシーン遷移するようにしました

---
+++ Unlucky/Assets/Member/katou/Scripts/test2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    [SerializeField] private Scene scene;
+
+    float second;
+    void Update()
+    {
+        
+        second += Time.deltaTime;
+        Debug.Log("second");
+        if(second > 10)
+        {
+            scene.LoadScene(SceneName.GameName.Scene3);
+        }
+    }
+}
