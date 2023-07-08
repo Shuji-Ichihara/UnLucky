010038cecbc6bba3867a2f00bb46a35b59399779
keisuke <kuma.0209@icloud.com>
2023-07-08 17:09:35 +0900

    GameManagerの完成

--- Unlucky/Assets/Member/Keisuke/Scripts/GameManager.cs
+++ Unlucky/Assets/Member/Keisuke/Scripts/GameManager.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-
+    // プレイヤーキャラ
     private Player1Controller _player1Controller;
     private Player2Controller _player2Controller;
-
+    //　マップ１～3を割り当てる
     [SerializeField]
     private List<GameObject> _map1Objects = new List<GameObject>();
     [SerializeField]
     private List<GameObject> _map2Objects = new List<GameObject>();
     [SerializeField]
     private List<GameObject> _map3Objects = new List<GameObject>();
-
+    //　ダメージ計算
     [SerializeField]
     private int _smallDamage = 10;
     [SerializeField]
     private int _largeDamage = 30;
 
     private int _unluckyNunber = 0;
+
+    private bool _checkDamagePlayer1 = false;
+    private bool _checkDamagePlayer2 = false;
 
     private void Awake()
     {
@@ -34,7 +38,6 @@ public class GameManger : MonoBehaviour
     void Update()
     {
         PlayerDamage();
-        CalculateDamage();
     }
 
     /// <summary>
@@ -51,41 +54,41 @@ public class GameManger : MonoBehaviour
 
     private void PlayerDamage()
     {
-        if (_player1Controller.IsEntried == true)
+        if (_player1Controller.IsEntried == true && _checkDamagePlayer1 == false)
         {
             switch (_player1Controller.GamePosition)
             {
                 case 0:
-                    //後で変更する
-                    Player1Controller.AccumulatedDamage += 10;
+                    CalculateDamagePlayer1();
+                    _checkDamagePlayer1 = true;
                     break;
                 case 1:
-                    //後で変更する
-                    Player1Controller.AccumulatedDamage += 10;
+                    CalculateDamagePlayer1();
+                    _checkDamagePlayer1 = true;
                     break;
                 case 2:
-                    //後で変更する
-                    Player1Controller.AccumulatedDamage += 10;
+                    CalculateDamagePlayer1();
+                    _checkDamagePlayer1 = true;
                     break;
                 default:
                     break;
             }
         }
-        if (_player2Controller.IsEntried == true)
+        if (_player2Controller.IsEntried == true && _checkDamagePlayer2 == false)
         {
             switch (_player2Controller.GamePosition)
             {
                 case 0:
-                    //後で変更する
-                    Player2Controller.AccumulatedDamage += 10;
+                    CalculateDamagePlayer2();
+                    _checkDamagePlayer2 = true;
                     break;
                 case 1:
-                    //後で変更する
-                    Player2Controller.AccumulatedDamage += 10;
+                    CalculateDamagePlayer2();
+                    _checkDamagePlayer2 = true;
                     break;
                 case 2:
-                    //後で変更する
-                    Player2Controller.AccumulatedDamage += 10;
+                    CalculateDamagePlayer2();
+                    _checkDamagePlayer2 = true;
                     break;
                 default:
                     break;
@@ -93,26 +96,32 @@ public class GameManger : MonoBehaviour
         }
     }
 
-    private void CalculateDamage()
+    private void CalculateDamagePlayer1()
     {
         if (_unluckyNunber == _player1Controller.GamePosition)
         {
             Player1Controller.AccumulatedDamage += _largeDamage;
+            Debug.Log($"{Player1Controller.AccumulatedDamage} : HP {100 - Player1Controller.AccumulatedDamage}");
         }
         else
         {
             Player1Controller.AccumulatedDamage += _smallDamage;
+            Debug.Log($"{Player1Controller.AccumulatedDamage} : HP {100 - Player1Controller.AccumulatedDamage}");
         }
+    }
 
-        if (_unluckyNunber == _player1Controller.GamePosition)
+    private void CalculateDamagePlayer2()
+    {
+        if (_unluckyNunber == _player2Controller.GamePosition)
         {
             Player2Controller.AccumulatedDamage += _largeDamage;
+            Debug.Log($"{Player2Controller.AccumulatedDamage}  : HP  {100 - Player2Controller.AccumulatedDamage}");
         }
         else
         {
             Player2Controller.AccumulatedDamage += _smallDamage;
+            Debug.Log($"{Player2Controller.AccumulatedDamage}  : HP  {100 - Player2Controller.AccumulatedDamage}");
         }
-
     }
 
 }
