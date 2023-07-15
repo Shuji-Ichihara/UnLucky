64049f0a8c47183bf3f3fbe4200577ba37a9cd08
Note-57\student <>
2023-07-15 10:29:49 +0900

    ひとまず上げ

---
+++ Unlucky/Assets/RockFolls.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockFolls : MonoBehaviour
+{
+    public float duration = 3f; // ˆÚ“®‚É‚©‚©‚éŠÔi•bj
+    public float distance = 1f; // ˆÚ“®‚·‚é‹——£
+
+    private float elapsedTime = 0f;
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        targetPosition = startPosition - new Vector3(0f, distance, 0f);
+        Invoke("StartCameraMovement", 3f);
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime <= duration)
+            {
+                float t = elapsedTime / duration;
+                transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            }
+            else
+            {
+                transform.position = targetPosition;
+                isMoving = false;
+            }
+        }
+    }
+
+    private void StartCameraMovement()
+    {
+        isMoving = true;
+    }
+}
